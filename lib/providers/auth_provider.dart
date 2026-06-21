3bc05f591b736aaa8c31bcb536c5916437f8f29c
JohnnyKozman2007 <121357226+JohnnyKozman2007@users.noreply.github.com>
2026-06-21 18:25:30 +0300

    Fix formatting in auth_provider.dart

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,3 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final twoFAVerifiedProvider = StateProvider<bool>((ref) => false);
+final twoFAVerifiedProvider = StateProvider<bool>((ref) => false);   
