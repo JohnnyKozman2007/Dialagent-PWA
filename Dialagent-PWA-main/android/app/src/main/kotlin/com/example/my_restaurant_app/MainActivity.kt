7c4784616e40fa4fe3d243835f27fab2639b6121
JohnnyKozman2007 <kozmanjohnny82@gmail.com>
2026-07-08 05:06:11 +0300

    cleanup: remove duplicate nested project folder

--- Dialagent-PWA-main/android/app/src/main/kotlin/com/example/my_restaurant_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_restaurant_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Dialagent-PWA-main/lib/providers/auth_provider.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final twoFAVerifiedProvider = StateProvider<bool>((ref) => false);   
